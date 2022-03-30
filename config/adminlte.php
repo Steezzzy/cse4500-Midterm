3723cce771fc009baf44d9bf599c0f607bf3d8ae
Steezzzy <99232091+Steezzzy@users.noreply.github.com>
2022-03-30 11:12:17 -0700

    a

--- config/adminlte.php
+++ config/adminlte.php
@@ -237,7 +237,7 @@ return [
         [
             'text' => 'Manage Buyers',
             'url'  => 'customer',
-            'icon' => 'fas fa-fw fa-User',
+            'icon' => 'fas fa-fw fa-users',
         ],
         [         
             'text' => 'Manage Equipment',
