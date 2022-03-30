925f1d9dfd65c252e973ffba32c1b9e1fa50d806
Steezzzy <99232091+Steezzzy@users.noreply.github.com>
2022-03-30 14:27:07 -0700

    Update create.blade.php

--- resources/views/buyer/create.blade.php
+++ resources/views/buyer/create.blade.php
@@ -1,8 +1,5 @@
 @extends('adminlte::page')
 
-@section('title', 'Buyers')
-
-@section('content_header')
-    <h1>List of Users who bought items</h1>
-@stop
-
+@section('content')
+    {!! form($form) !!}
+@endsection
