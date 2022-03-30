5e41d2593e5f1174e851fd2f1f1921162c193eee
Steezzzy <99232091+Steezzzy@users.noreply.github.com>
2022-03-30 14:40:49 -0700

    Update list.blade.php

--- resources/views/buyer/list.blade.php
+++ resources/views/buyer/list.blade.php
@@ -5,3 +5,27 @@
 @section('content_header')
     <h1>List of Users who bought items</h1>
 @stop
+
+@section('content')
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Name</th><th style="width: 40px">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($buyer AS $buyer)
+        <tr>
+          <td>{{ $buyer->id }}</td>
+          <td>{{ $buyer->name }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('buyer.show',['buyer'=>$buyer->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('buyer.create') }} " class="btn btn-primary" >Create</a>
+@stop
