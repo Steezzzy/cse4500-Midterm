0f5af612efacbcdd913e1c7ce661aabcbbfb6ea1
Steezzzy <99232091+Steezzzy@users.noreply.github.com>
2022-03-30 17:50:49 -0700

    Update NotesController.php

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -37,7 +37,7 @@ class NotesController extends Controller
 
     public function show($id)
     {
-        $notes= Notes::find($id); 
-        return view('notes.detail',compact('notes'));
+        $note= Notes::find($id); 
+        return view('notes.detail',compact('note'));
     }
 }
