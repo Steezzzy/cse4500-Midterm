1908b58659e956f77ccd4a0352593a2e80c397ec
Steezzzy <99232091+Steezzzy@users.noreply.github.com>
2022-03-30 17:52:57 -0700

    Update list.blade.php

--- resources/views/notes/list.blade.php
+++ resources/views/notes/list.blade.php
@@ -12,14 +12,15 @@
     <table id="table" class="table table-bordered sortable">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Name</th><th style="width: 40px">Action</th>
+          <th style="width: 10px">#</th><th>ID</th><th>Notes</th><th>Software</th><th style="width: 40px">Action</th>
         </tr>
       </thead>
       <tbody>
         @foreach($notes AS $note)
         <tr>
           <td>{{ $note->id }}</td>
-          <td>{{ $note->name }}</td>
+          <td>{{ $note->notes }}</td>
+          <td>{{ $note->software }}</td>
           <td><a class="btn btn-default btn-sm" href="{{ route('note.show',['note'=>$note->id]) }}">View</a></td>
         </tr>
         @endforeach
